24e10dcfd069e54eabbfe3f961f97341bf1624c1
Luiz Rioja <lulyrioja@gmail.com>
2021-07-14 16:50:10 -0300

    :alien: Applying Singleton pattern to category and specification repositories

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,11 +1,21 @@
-import { Category } from "../model/Category";
-import { ICreateCategoryDTO } from "./ICategoriesRepository";
+import { Category } from "../../model/Category";
+import { ICreateCategoryDTO } from "../ICategoriesRepository";
 
 class CategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ description, name }: ICreateCategoryDTO): void {
