27274e65e191899f03e21582fb758677fbeb70b2
Luiz Rioja <lulyrioja@gmail.com>
2021-07-14 18:48:04 -0300

    :alien: Creating Category Import Route with csv

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const categoriesRoutes = Router();
 
@@ -11,6 +17,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
