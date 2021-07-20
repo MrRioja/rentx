b2034fe421ade556eb23bb4429b2680bf4de0ba8
Luiz Rioja <lulyrioja@gmail.com>
2021-07-20 00:31:12 -0300

    :alien: Changing models to entities

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,11 +1,11 @@
-import { Category } from "../../model/Category";
+import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 class ListCategoriesUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  execute(): Category[] {
-    const categories = this.categoriesRepository.list();
+  async execute(): Promise<Category[]> {
+    const categories = await this.categoriesRepository.list();
 
     return categories;
   }
