d21f2a5f20892b8f90b92558644e364d657d3e17
Luiz Rioja <lulyrioja@gmail.com>
2021-07-21 00:05:57 -0300

    :alien: Registering repositories in the tsyringe container and importing it into the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
+import "./shared/container";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
