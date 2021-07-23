bcb5ad3d929633fc9f0a6e59a869cca7407956d3
Luiz Rioja <lulyrioja@gmail.com>
2021-07-23 00:20:54 -0300

    :alien: Creating function to remove user avatar

---
+++ src/utils/file.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string): Promise<void> => {
+  try {
+    await fs.promises.stat(filename);
+  } catch (error) {
+    return;
+  }
+  await fs.promises.unlink(filename);
+};
