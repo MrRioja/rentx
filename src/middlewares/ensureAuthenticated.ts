41c5eb737d357621f9829c52ecedc52f8efb32e2
Luiz Rioja <lulyrioja@gmail.com>
2021-07-23 00:23:42 -0300

    :alien: Creating authentication middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "98df85ab9438ef5bb7b19f5d32e5dd67"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists", 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch (error) {
+    throw new AppError("Invalid token", 401);
+  }
+}
