8559f7c2b7db678cc0e7d1147d9b02e5cc0cbc27
Luiz Rioja <lulyrioja@gmail.com>
2021-07-23 00:27:55 -0300

    :alien: Creating function to search for users by id

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -16,12 +16,16 @@ class UsersRepository implements IUsersRepository {
     email,
     password,
     driver_license,
+    avatar,
+    id,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       password,
       driver_license,
+      avatar,
+      id,
     });
 
     await this.repository.save(user);
@@ -29,6 +33,12 @@ class UsersRepository implements IUsersRepository {
 
   async findByEmail(email: string): Promise<User> {
     const user = await this.repository.findOne({ email });
+
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.repository.findOne(id);
 
     return user;
   }
