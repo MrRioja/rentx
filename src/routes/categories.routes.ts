89aebfe0e9275a6d4917de27827ddde475c9413b
Luiz Rioja <lulyrioja@gmail.com>
2021-07-23 00:26:18 -0300

    :alien: Refactoring file upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
-
-const upload = multer({
-  dest: "./tmp",
-});
 
 const categoriesRoutes = Router();
 
@@ -15,13 +12,15 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp"));
+
 categoriesRoutes.post("/", createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
-  upload.single("file"),
+  uploadAvatar.single("file"),
   importCategoryController.handle
 );
 
