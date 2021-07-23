4035ac63bfc5823341feb03f5618a6da0c9fad60
Luiz Rioja <lulyrioja@gmail.com>
2021-07-22 21:18:29 -0300

    :alien: Creating repository, controller, usecase and user entity

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
