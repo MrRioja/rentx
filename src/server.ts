0824bf68711c7b858da2c58c56236f0e9da21fc6
Luiz Rioja <lulyrioja@gmail.com>
2021-07-23 00:23:02 -0300

    :alien: Adding custom bugs to the application

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
+
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,5 +17,18 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: `Internal Server Error - ${error.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running"));
