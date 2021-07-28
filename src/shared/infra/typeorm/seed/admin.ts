7a823a4861b705f29575b788724662c78163704a
Luiz Rioja <lulyrioja@gmail.com>
2021-07-27 23:24:53 -0300

    :alien: Creating the admin user seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,22 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO users(id, name, email, password, "isAdmin", created_at, driver_license)
+    VALUES ('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')`
+  );
+
+  await connection.close();
+}
+
+create().then(() => {
+  console.log("User admin created");
+});
