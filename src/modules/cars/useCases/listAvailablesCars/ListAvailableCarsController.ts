a25c3f32612ba22646dd39123f1af882df7be7e9
Luiz Rioja <lulyrioja@gmail.com>
2021-07-28 22:38:07 -0300

    :alien: Creating available car listing controller

---
+++ src/modules/cars/useCases/listAvailablesCars/ListAvailableCarsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase";
+
+class ListAvailableCarsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { category_id, name, brand } = request.query;
+
+    const listAvailableCarsUseCase = container.resolve(
+      ListAvailableCarsUseCase
+    );
+
+    const cars = await listAvailableCarsUseCase.execute({
+      name: name as string,
+      brand: brand as string,
+      category_id: category_id as string,
+    });
+
+    return response.json(cars);
+  }
+}
+
+export { ListAvailableCarsController };
