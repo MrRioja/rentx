7b5da786326d9ea5b8f9b080a57bb39b2ac9429b
Luiz Rioja <lulyrioja@gmail.com>
2021-07-30 00:10:25 -0300

    :alien: Creating controller and UseCase from uploadCarImage

---
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UploadCarImageUseCase } from "./UploadCarImageUseCase";
+
+interface IFiles {
+  filename: string;
+}
+
+class UploadCarImageController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const images = request.files as IFiles[];
+
+    const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
+
+    const images_name = images.map((file) => file.filename);
+
+    await uploadCarImageUseCase.execute({ car_id: id, images_name });
+
+    return response.status(201).json();
+  }
+}
+
+export { UploadCarImageController };
