268c19b7db1dba48c0a307564086e1cb88804fbe
Luiz Rioja <lulyrioja@gmail.com>
2021-07-29 23:56:59 -0300

    :alien: Fixing uuid aliases in category and spec models

--- src/modules/cars/infra/typeorm/entities/Category.ts
+++ src/modules/cars/infra/typeorm/entities/Category.ts
@@ -1,5 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidV4 } from "uuid";
 
 @Entity("categories")
 class Category {
@@ -17,7 +17,7 @@ class Category {
 
   constructor() {
     if (!this.id) {
-      this.id = uuidv4();
+      this.id = uuidV4();
     }
   }
 }
--- src/modules/cars/infra/typeorm/entities/Specification.ts
+++ src/modules/cars/infra/typeorm/entities/Specification.ts
@@ -1,5 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidV4 } from "uuid";
 
 @Entity("specifications")
 class Specification {
@@ -17,7 +17,7 @@ class Specification {
 
   constructor() {
     if (!this.id) {
-      this.id = uuidv4();
+      this.id = uuidV4();
     }
   }
 }
