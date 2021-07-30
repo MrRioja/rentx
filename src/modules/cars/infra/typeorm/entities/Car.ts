92f5b30d7ec451b87efe11e2c37f4a00f89f6c61
Luiz Rioja <lulyrioja@gmail.com>
2021-07-29 23:58:01 -0300

    :alien: Fixing column name in inverseJoinColumns

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -50,7 +50,7 @@ class Car {
   @JoinTable({
     name: "specifications_cars",
     joinColumns: [{ name: "car_id" }],
-    inverseJoinColumns: [{ name: "specifications_id" }],
+    inverseJoinColumns: [{ name: "specification_id" }],
   })
   specifications: Specification[];
 
