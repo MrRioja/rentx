16f9d1da298e42dfb3a6a56a2ce291fb94bc79ce
Luiz Rioja <lulyrioja@gmail.com>
2021-07-30 00:03:54 -0300

    :alien: Creating the car model and image repositories

---
+++ src/modules/cars/repositories/in-memory/CarsImagesRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { getRepository, Repository } from "typeorm";
+
+import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
+
+import { ICarsImagesRepository } from "../ICarsImagesRepository";
+
+class CarsImagesRepositoryInMemory implements ICarsImagesRepository {
+  private repository: Repository<CarImage>;
+
+  constructor() {
+    this.repository = getRepository(CarImage);
+  }
+
+  async create(car_id: string, image_name: string): Promise<CarImage> {
+    const carImage = this.repository.create({ car_id, image_name });
+
+    await this.repository.save(carImage);
+
+    return carImage;
+  }
+}
+
+export { CarsImagesRepositoryInMemory };
