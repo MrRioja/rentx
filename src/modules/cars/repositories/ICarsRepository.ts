5cede0e83b5bd9886d1692d1d9943e935f924b47
Luiz Rioja <lulyrioja@gmail.com>
2021-08-03 23:57:32 -0300

    :alien: Creating the Car Availability Update Method

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -5,6 +5,7 @@ interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
   findById(id: string): Promise<Car>;
+  updateAvailable(id: string, available: boolean): Promise<void>;
   findAvailable(
     brand?: string,
     category_id?: string,
