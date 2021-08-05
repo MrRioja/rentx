57ce08eda66560973ae7a93295b79663e694a769
Luiz Rioja <lulyrioja@gmail.com>
2021-08-04 23:45:56 -0300

    :alien: Creating the compare method in DateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   dateNow(): Date;
   convertToUTC(date: Date): string;
   compareInHours(start_date: Date, end_date: Date): number;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };
