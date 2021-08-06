1ff8715c837c138503767e94bc4f766ab648e5e6
Luiz Rioja <lulyrioja@gmail.com>
2021-08-05 23:57:08 -0300

    :alien: Creating token variables configuration file

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  secret_token: "98df85ab9438ef5bb7b19f5d32e5dd67",
+  secret_refresh_token: "0ceb1b874fd498845d0ef679b54f59f3",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+};
