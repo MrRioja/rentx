49ea163882ecfadc3e99f9b058a81d2e9227924c
Luiz Rioja <lulyrioja@gmail.com>
2021-08-05 23:59:02 -0300

    :alien: Creating day addition method in the date provider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   convertToUTC(date: Date): string;
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
