c98a6ba81fe5bbc485d681bd147b02aae411027d
Luiz Rioja <lulyrioja@gmail.com>
2021-08-06 00:11:32 -0300

    :alien: Renaming the user token table

--- src/shared/infra/typeorm/migrations/1628215060415-CreateUsersToken.ts
+++ src/shared/infra/typeorm/migrations/1628215060415-CreateUsersToken.ts
@@ -4,7 +4,7 @@ export class CreateUsersToken1628215060415 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users_token",
+        name: "users_tokens",
         columns: [
           {
             name: "id",
