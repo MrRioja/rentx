39ce6ddf965bef8ce9bc1906e03a5aaf8298aec4
Luiz Rioja <lulyrioja@gmail.com>
2021-08-06 23:48:45 -0300

    :alien: Creating the method of adding hours in the DateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -4,6 +4,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
   addDays(days: number): Date;
+  addHours(hours: number): Date;
 }
 
 export { IDateProvider };
