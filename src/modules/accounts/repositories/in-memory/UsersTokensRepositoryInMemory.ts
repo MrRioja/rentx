e76139d1b14f50576c3c204264fd168bb21d01e5
Luiz Rioja <lulyrioja@gmail.com>
2021-08-07 20:00:37 -0300

    :alien: Creating UsersTokens and MailProvider Memory Repositories

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,47 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  usersTokens: UserTokens[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+
+    Object.assign(userToken, { expires_date, refresh_token, user_id });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    return this.usersTokens.find(
+      (userToken) =>
+        userToken.user_id === user_id &&
+        userToken.refresh_token === refresh_token
+    );
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const userToken = this.usersTokens.find((userToken) => userToken.id === id);
+
+    this.usersTokens.splice(this.usersTokens.indexOf(userToken));
+  }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    return this.usersTokens.find(
+      (userToken) => userToken.refresh_token === refresh_token
+    );
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
