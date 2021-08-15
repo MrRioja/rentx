11b7ca3ad00ac23770087117dbbf0ad870960295
Luiz Rioja <lulyrioja@gmail.com>
2021-08-14 23:25:52 -0300

    :alien: configuring babel

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@modules": ["./src/modules"],
+          "@config": ["./src/config"],
+          "@shared": ["./src/shared"],
+          "@utils": ["./src/utils"],
+          "@errors": ["./src/errors"],
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
