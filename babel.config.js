e1f02fdf65f0e65d57135993544237de8b895195
Luiz Rioja <lulyrioja@gmail.com>
2021-08-15 00:06:25 -0300

    :alien: Fixing babel configuration

--- babel.config.js
+++ babel.config.js
@@ -8,11 +8,11 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@modules": ["./src/modules"],
-          "@config": ["./src/config"],
-          "@shared": ["./src/shared"],
-          "@utils": ["./src/utils"],
-          "@errors": ["./src/errors"],
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@shared": "./src/shared",
+          "@utils": "./src/utils",
+          "@errors": "./src/errors",
         },
       },
     ],
