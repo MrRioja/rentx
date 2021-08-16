768ea478171fecf2e0bbffb14673c94149dabff4
Luiz Rioja <lulyrioja@gmail.com>
2021-08-15 22:21:29 -0300

    :alien: Configuring Sentry in our application

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,8 @@ import swaggerUi from "swagger-ui-express";
 
 import "@shared/container";
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
@@ -19,6 +21,19 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -28,6 +43,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
